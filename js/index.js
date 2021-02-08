7b64f4bd63efdbfcfc9bfc924c03029b125661b0
VHromyk <hromykk@gmail.com>
2021-02-09 01:27:41 +0200

    finished project

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const menuOpenRef = document.querySelector(".menu-btn-open");
 const menuCloseRef = document.querySelector(".menu-btn-close");
 const headerWrapRef = document.querySelector(".header-nav-wrap");
+const ourPerformanceRef = document.querySelector(".our-performance");
 
 menuOpenRef.addEventListener("click", () => {
   headerWrapRef.classList.toggle("is-close");
@@ -11,3 +12,9 @@ menuCloseRef.addEventListener("click", () => {
   headerWrapRef.classList.toggle("is-close");
   headerWrapRef.classList.toggle("is-open");
 });
+
+if (document.documentElement.clientWidth > 1919) {
+  ourPerformanceRef.classList.remove("container-box");
+  ourPerformanceRef.classList.add("large-container");
+  console.log(document.documentElement.clientWidth);
+}
--- js/siema.js
+++ js/siema.js
@@ -1,33 +1,32 @@
-let countPage = 0;
+const widthWiewPort = document.documentElement.clientWidth;
 
-if (document.documentElement.clientWidth < 576) {
+if (widthWiewPort < 576) {
   countPage = 1;
-} else if (
-  document.documentElement.clientWidth > 576 &&
-  document.documentElement.clientWidth < 767
-) {
+} else if (widthWiewPort > 576 && widthWiewPort < 767) {
   countPage = 2;
-} else if (
-  document.documentElement.clientWidth > 768 &&
-  document.documentElement.clientWidth < 1919
-) {
+} else if (widthWiewPort > 768 && widthWiewPort < 1919) {
   countPage = 3;
 } else {
   countPage = 4;
 }
 
+console.log(countPage);
 const mySiema = new Siema({
   selector: ".large-slider",
   perPage: countPage,
 });
 
-console.log(mySiema);
-
-const myNewSiema = new Siema({
-  selector: ".front-carousel",
+const ourPerformance = new Siema({
+  selector: ".our-performance-carousel",
   duration: 200,
   easing: "cubic-bezier(.17,.67,.32,1.34)",
-  perPage: 1,
+  perPage: {
+    576: 1.3, // 2 items for viewport wider than 800px
+    730: 1.6, // 2 items for viewport wider than 800px
+    1240: 1.9, // 3 items for viewport wider than 1240px
+    1919: 4, // 3 items for viewport wider than 1240px
+    2200: 5.5, // 3 items for viewport wider than 1240px
+  },
   startIndex: 0,
   draggable: true,
   multipleDrag: true,
